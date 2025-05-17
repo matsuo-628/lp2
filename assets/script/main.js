50a625c277db60f66d4a33edd4c2a0afb59249ea
matsuo628 <lit.me.23628@gmail.com>
2025-05-17 23:59:14 +0900

    lp2

---
+++ assets/script/main.js
@@ -0,0 +1,45 @@
+// QandAのToggle menu===========================
+$('.qa__item_question').on('click', function () {
+    const $item = $(this).closest('.qa__item');
+    $item.toggleClass('open');
+    $item.find('.qa__item_answer').slideToggle(200);
+});
+
+//slickスライダーの指定==========================
+$(document).ready(function(){
+    $('.voices__slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+    });
+});
+//トップへ戻るボタン===========================
+const backToTopButton = document.getElementById("toTopButton");
+//ボタンが表示される条件
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 600) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+//ボタンをクリックしたときの動作
+backToTopButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
